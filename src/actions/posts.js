d17546d5617409311d515f6d83a5d4c98f230d8d
yushan <yushan@intervest.lk>
2021-03-28 18:18:38 +0530

    feat: Create action type constants related to posts portion in state

---
+++ src/actions/posts.js
@@ -0,0 +1,5 @@
+export const POSTS_GET = 'POSTS_GET';
+export const POST_UPVOTE = 'POST_UPVOTE';
+export const POST_DOWNVOTE = 'POST_DOWNVOTE';
+export const POST_ADD = 'POST_ADD';
+export const POST_UPDATE = 'POST_UPDATE';
