ddb03ead8b21fbb288b67283490afe77c4507f44
yushan <yushan@intervest.lk>
2021-03-28 23:21:50 +0530

    feat: get, getPost, votePost API functions created

---
+++ src/utils/API.js
@@ -0,0 +1,33 @@
+const api = "http://localhost:3001";
+
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+    'Accept': 'application/json',
+    'Authorization': token
+}
+
+export const get = () =>
+    fetch(`${api}/posts/`, { headers })
+        .then(res => res.json())
+        .then(data => data)
+
+export const getPost = (postId) =>
+    fetch(`${api}/posts/${postId}`, { headers })
+        .then(res => res.json())
+        .then(data => console.log(data))
+
+export const votePost = (postId, option) =>
+    fetch(`${api}/posts/${postId}`, {
+        method: "POST",
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            option
+        })
+    })
+        .then(res => res.json())
