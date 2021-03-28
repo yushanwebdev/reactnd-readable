e023758ed47a623024d75a616cc0fbf12254e8ea
yushan <yushan@intervest.lk>
2021-03-28 23:25:17 +0530

    test: Test API functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
+import { Component } from 'react';
+import * as API from './utils/API';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    console.log(API.votePost('8xf0y6ziyjabvozdd253nd', 'upVote'));
+  }
+
+  render() {
+    return(
+      <div>
+        App
+      </div>
+    )
+  }
 }
 
 export default App;
