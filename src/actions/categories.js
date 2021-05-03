72798e50995767e3d5363026259df53ab458ea70
yushan <yushan@intervest.lk>
2021-05-03 12:34:25 +0530

    feat: Develop categories related actions

--- src/actions/categories.js
+++ src/actions/categories.js
@@ -1,1 +1,28 @@
-export const CATEGORIES_GET = 'CATEGORIES_GET';
+import { getCategories, getCategoryPosts } from "../utils/API";
+
+export const CATEGORIES_GET = "CATEGORIES_GET";
+export const CATEGORY_POSTS = "CATEGORY_POSTS";
+
+function fetchCats(cats) {
+  return {
+    type: CATEGORIES_GET,
+    cats,
+  };
+}
+
+export function handleFetchCats() {
+  return (dispatch) =>
+    getCategories().then((cats) => dispatch(fetchCats(cats)));
+}
+
+function fetchCatPosts(posts) {
+  return {
+    type: CATEGORY_POSTS,
+    posts,
+  };
+}
+
+export function handleFetchCatPosts(path) {
+  return (dispatch) =>
+    getCategoryPosts(path).then((posts) => dispatch(fetchCatPosts(posts)));
+}
