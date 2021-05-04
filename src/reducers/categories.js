7c7433682120d382ee825cefc5eb68b4da25b8ef
yushan <yushan@intervest.lk>
2021-05-04 11:52:11 +0530

    refactor: Remove an unnecessary case in category reducer

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -1,17 +1,9 @@
-import { CATEGORIES_GET, CATEGORY_POSTS } from "../actions/categories";
+import { CATEGORIES_GET } from "../actions/categories";
 
-export default function categories(state = {}, action) {
+export default function categories(state = [], action) {
   switch (action.type) {
     case CATEGORIES_GET:
-      return {
-        ...state,
-        ...action.cats,
-      };
-    case CATEGORY_POSTS:
-      return {
-        ...state,
-        ...action.posts,
-      };
+      return action.cats;
     default:
       return state;
   }
