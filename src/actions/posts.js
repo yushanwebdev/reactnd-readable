8ea6fc95c03c696aa1bec16620055082101e11b3
yushan <yushan@intervest.lk>
2021-05-24 10:22:22 +0530

    feat: Update the post related actions with error catching

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -5,37 +5,38 @@ import {
   updatePost,
   getCategoryPosts,
 } from "../utils/API";
+import { addError } from "./errors";
 
 export const POSTS_GET = "POSTS_GET";
 export const POST_UPDATE = "POST_UPDATE";
+export const POST_VOTE = "POST_VOTE";
 
 function addStorePosts(posts) {
   return {
     type: POSTS_GET,
-    posts,
+    payload: posts,
   };
 }
 
 export function handleFetchPosts() {
   return (dispatch) =>
-    getPosts().then((posts) => dispatch(addStorePosts(posts)));
+    getPosts()
+      .then((posts) => dispatch(addStorePosts(posts)))
+      .catch((e) => addError(e));
 }
 
 export function handleFetchCatPosts(path) {
   return (dispatch) =>
-    getCategoryPosts(path).then((posts) => dispatch(addStorePosts(posts)));
+    getCategoryPosts(path)
+      .then((posts) => dispatch(addStorePosts(posts)))
+      .catch((e) => addError(e));
 }
 
 function updateStorePost(post) {
   return {
     type: POST_UPDATE,
-    post,
+    payload: post,
   };
-}
-
-export function handleVotePost(id, option) {
-  return (dispatch) =>
-    votePost(id, option).then((post) => dispatch(updateStorePost(post)));
 }
 
 export function handleAddPost(data) {
@@ -46,4 +47,26 @@ export function handleAddPost(data) {
 export function handleUpdatePost(data) {
   return (dispatch) =>
     updatePost(data).then((post) => dispatch(updateStorePost(post)));
+}
+
+function changeVotePost(payload) {
+  return {
+    type: POST_VOTE,
+    payload,
+  };
+}
+
+export function handleVotePost(vote) {
+  return (dispatch) => {
+    dispatch(changeVotePost(vote));
+    return votePost(vote).catch((e) => {
+      addError(e);
+      dispatch(
+        changeVotePost({
+          ...vote,
+          option: vote.option === "upVote" ? "downVote" : "upVote",
+        })
+      );
+    });
+  };
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,6 +1,7 @@
-import { POSTS_GET, POST_UPDATE } from "../actions/posts";
+import { POSTS_GET, POST_UPDATE, POST_VOTE } from "../actions/posts";
 
-export default function posts(state = [], action) {
+export default function posts(state = {}, action) {
+  const { id, option } = action.payload;
   switch (action.type) {
     case POSTS_GET:
       return action.posts;
@@ -8,6 +9,15 @@ export default function posts(state = [], action) {
       return state.map((item) =>
         item.id === action.post.id ? action.post : item
       );
+    case POST_VOTE:
+      return {
+        ...state,
+        [id]: {
+          ...state[id],
+          voteScore:
+            option === "upVote" ? ++state[id].voteScore : --state[id].voteScore,
+        },
+      };
     default:
       return state;
   }
